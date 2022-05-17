061c51d6b721ed3e5a02c8d7eada23f7f00dc4b1
tsukasaI <inoue.tsukasa.4@gmail.com>
2022-05-17 09:47:57 +0900

    gin checked

--- src/main.go
+++ src/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	engine := gin.Default()
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+	engine.Run(":8080")
 }
