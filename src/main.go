a5ed0b73ab37eb264df428987529e5c097c34e9c
tsukasaI <inoue.tsukasa.4@gmail.com>
2022-05-18 17:50:09 +0900

    home dev started. DB fetch for status code 200 done

--- src/main.go
+++ src/main.go
@@ -9,32 +9,71 @@ import (
 	"gorm.io/gorm"
 )
 
-type User struct {
-	Id        int `gorm:"column:id"`
-	Name      string
-	Email     string
-	Password  string
-	CreatedAt string
-	UpdatedAt string
-}
-
 func main() {
 	engine := gin.Default()
+
 	engine.GET("/", func(c *gin.Context) {
+		db, err := connect()
+		fmt.Println(db)
+		fmt.Println(err)
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+	engine.GET("/home", func(c *gin.Context) {
+		userId := c.Query("userId")
+		fmt.Printf("%v\n", userId)
+		err := getHome(userId)
+		fmt.Printf("%v\n", err)
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello world",
 		})
 	})
 
+	engine.Run(":8080")
+}
+
+// database.go
+func connect() (*gorm.DB, error) {
 	dsn := "docker:docker@tcp(db:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	fmt.Printf("%v\n", db)
-	fmt.Printf("%v\n", err)
+	return db, err
+}
 
-	var users []User
-	db.Find(&users)
-	fmt.Printf("%v\n", users)
+// user.go
+type UserFollow struct {
+	UserID uint
+	FollowId uint
+}
 
-	engine.Run(":8080")
+type Post struct {
+	Id uint `gorm:"primaryKey"`
+	Comment string
+	UserID uint
+	User User
+	CreatedAt string
+	UpdatedAt string
+}
+
+type User struct {
+	Id        int `gorm:"primaryKey"`
+	Name      string
+	Email     string
+	// Password  string
+	CreatedAt string
+	UpdatedAt string
+}
+
+
+func getHome (userId string) (error) {
+	db, err := connect()
+	if err != nil {
+		return err
+	}
+	var posts []Post
+	subQuery := db.Select("`follow_id`").Where("user_id = ?", userId).Table("user_follows")
+	db.Where("`user_id` in (?)", subQuery).Or("user_id = ?", userId).Preload("User").Find(&posts)
+	fmt.Printf("%v\n", posts)
+	return nil
 }
