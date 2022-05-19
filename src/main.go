ad9628010b4c32a94712befea3e3397e15e968f9
tsukasaI <inoue.tsukasa.4@gmail.com>
2022-05-19 16:45:07 +0900

    response done

--- src/main.go
+++ src/main.go
@@ -8,6 +8,11 @@ import (
 	"gorm.io/gorm"
 )
 
+type QueryParam struct {
+	// userIdはintで指定してほしい テスト仕様書の変更したほうがよい。 取り急ぎコメント
+	UserId int `form:"userId" binding:"required"`
+}
+
 func main() {
 	engine := gin.Default()
 
@@ -16,21 +21,50 @@ func main() {
 			"message": "ok",
 		})
 	})
-	engine.GET("/home", func(c *gin.Context) {
-		userId := c.Query("userId")
-		posts, err := getHome(userId)
-		if err != nil {
-			c.JSON(500, gin.H{
-				"data": err,
-			})
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"data": posts,
-		})
-	})
+	engine.GET("/home", getHome)
 
 	engine.Run(":8080")
 }
+
+func getHome(c *gin.Context) {
+	var queryParam QueryParam
+	if c.ShouldBind(&queryParam) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "userを指定してください。"})
+		return
+	}
+	userId := c.Query("userId")
+	db, err := connect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	user, err := findUser(db, userId)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "userが存在しません。"})
+		return
+	}
+	posts, err := getHomeData(db, user)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": posts,
+	})
+}
+
+func findUser(db *gorm.DB, userId string) (User, error) {
+	var user User
+	if err := db.Preload("Follows").First(&user, userId).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
 
 // database.go
 func connect() (*gorm.DB, error) {
@@ -47,32 +81,35 @@ type UserFollow struct {
 }
 
 type Post struct {
-	Id        uint `gorm:"primaryKey"`
-	Comment   string
-	UserID    uint
-	User      User
-	CreatedAt string
-	UpdatedAt string
+	gorm.Model
+	Comment string `gorm:"type:varchar(200) not null"`
+	UserID  uint
+	User    User
 }
 
 type User struct {
-	Id        int `gorm:"primaryKey"`
-	Name      string
-	Email     string
-	Password  string
-	CreatedAt string
-	UpdatedAt string
+	gorm.Model
+	Name     string `gorm:"type:varchar(50) not null"`
+	Email    string `gorm:"type:varchar(100) not null unique"`
+	Password string `gorm:"type:varchar(255) not null"`
+	Posts    []Post
+	Follows  []User `gorm:"many2many:user_follows"`
 }
 
 // home.go
-func getHome(userId string) ([]Post, error) {
-	db, err := connect()
-	if err != nil {
-		return nil, err
-	}
+func getHomeData(db *gorm.DB, user User) ([]Post, error) {
+	followIds := getFollowIds(user)
+
 	var posts []Post
-	subQuery := db.Select("`follow_id`").Where("user_id = ?", userId).Table("user_follows")
-	db.Where("`user_id` in (?)", subQuery).Or("user_id = ?", userId).Preload("User").Find(&posts)
+	db.Where("`user_id` in ?", followIds).Or("user_id = ?", user.ID).Preload("User").Find(&posts)
 
 	return posts, nil
+}
+
+func getFollowIds(user User) []uint {
+	var followIds []uint
+	for _, followUser := range(user.Follows) {
+		followIds = append(followIds, followUser.ID)
+	}
+	return followIds
 }
